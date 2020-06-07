5e1869b55e9da59dc3cc4db84c64fa3564e0db08
emilyolafson <emily.olafson@mail.mcgill.ca>
2020-06-07 14:05:37 -0400

    fixed linux fsl dir

--- controls_calculate_ICC.m
+++ controls_calculate_ICC.m
@@ -3,17 +3,14 @@
 % change test. 
 clear all;
 
-if(isempty(which('save_avw')))
-    addpath([getenv('FSLDIR') '/etc/matlab']);
-end
-
-setenv( 'FSLDIR', '/usr/local/fsl');
+addpath([getenv('FSLDIR') '/etc/matlab']);
+setenv( 'FSLDIR', '/usr/share/fsl/5.0');
 fsldir = getenv('FSLDIR');
 fsldirmpath = sprintf('%s/etc/matlab',fsldir);
 path(path, fsldirmpath);
 clear fsldir fsldirmpath;
 
-
+sub_direct='/home/emo4002/colossus_shared3/pons_sfmodelling/control_subs/';
 %% Global signal regression and ICC calculation for concatenated scans
 for i=24:47 %loop over subjects   
     %load Harvard-Oxford cortical atlas.
@@ -24,68 +21,66 @@ for i=24:47 %loop over subjects
     GM_reshape=reshape(GM, [902629 1]);
     GM_reshape(GM_reshape > 0.25) = 1; %threshold GM mask
     GM_reshape(GM_reshape <=0.25) = 0;
+    
     % get mask for regional analyses
     mask=getMask('cortex');
     
-    for j=1:nsess(i) %loop over sessions
-        cat_func=[]; %empty functional connectivity
-        cat_outliers=[]; %empty outliers matrix
-        nscans=ones(5, 24)+1;
-        cat_func = loadScans(i, j, nscans);
-        outlierframes = scrubMotionFrames(i, j, nscans);
-        
-        ts=cat_func.'; %flip to <time> x <voxels>
-        ts_GM = ts(:,GM==1);% <time> x <mask voxels> 
-
-        %global signal regression    
-        meants=mean(ts_GM,2); %mean across all gray matter voxels (i.e. where mask == 1)
-        confounds=[meants [0; diff(meants)] outlierframes]; %confounds list
-        Q=eye(size(confounds,1))-confounds*pinv(confounds);
-        ts_GSR=Q*(ts_GM); %global signal regressed time series
-        
-        %ts_clean = high motion frames excluded.
-        ts_clean_GSR=ts_GSR(sum(outlierframes,2)==0,:); %timeseries with motion frames excluded, and global signal regressed.            
-        %ts_clean=ts_GM(sum(outlierframes,2)==0,:); %timeseries wiith motion frames excluded. no GSR.
-
-        disp(strcat('saving ICC for ', subject, ' session ', num2str(j)))
-        
-        %save voxelwise measure of ICC.
-        ICC_GSR_cat_controls{i}{j}{k}=intrinsic_connectivity_contrast(ts_clean_GSR); %save timeseries into larger cell matrix
+    j=1;
     
-        % Make 3D image (for visualization)
-        p=1; %counter
-        for z=1:size(GM,1) %starts with the gray matter mask(which has all voxels in the 3d volume.)
-            % makes gray matter voxels equal to their ICC value.
-            if GM(z)==1
-                ICC_GSR(z)=ICC_GSR_cat_controls{i}{j}{k}(p); 
-                p=p+1;
-            else
-                ICC_GSR(z) =0;
-            end
+    cat_func=[]; %empty functional connectivity
+    cat_outliers=[]; %empty outliers matrix
+    nscans=ones(1, 24)+1;
+    cat_func = loadScans(i, j, nscans, sub_direct);
+    outlierframes = scrubMotionFrames(i, j, nscans);
+
+    ts=cat_func.'; %flip to <time> x <voxels>
+    ts_GM = ts(:,GM==1);% <time> x <mask voxels> 
+
+    %global signal regression    
+    meants=mean(ts_GM,2); %mean across all gray matter voxels (i.e. where mask == 1)
+    confounds=[meants [0; diff(meants)] outlierframes]; %confounds list
+    Q=eye(size(confounds,1))-confounds*pinv(confounds);
+    ts_GSR=Q*(ts_GM); %global signal regressed time series
+
+    %ts_clean = high motion frames excluded.
+    ts_clean_GSR=ts_GSR(sum(outlierframes,2)==0,:); %timeseries with motion frames excluded, and global signal regressed.            
+    %ts_clean=ts_GM(sum(outlierframes,2)==0,:); %timeseries wiith motion frames excluded. no GSR.
+
+    disp(strcat('saving ICC for ', subject, ' session ', num2str(j)))
+
+    %save voxelwise measure of ICC.
+    ICC_GSR_cat_controls{i}=intrinsic_connectivity_contrast(ts_clean_GSR); %save timeseries into larger cell matrix
+
+    % Make 3D image (for visualization)
+    p=1; %counter
+    for z=1:size(GM,1) %starts with the gray matter mask(which has all voxels in the 3d volume.)
+        % makes gray matter voxels equal to their ICC value.
+        if GM(z)==1
+            ICC_GSR(z)=ICC_GSR_cat_controls{i}(p); 
+            p=p+1;
+        else
+            ICC_GSR(z) =0;
         end
-        ICC_GSR_3D = reshape(ICC_GSR,91,109,91);
-        save_avw(ICC_GSR_3D,strcat('/home/emo4002/colossus_shared3/pons_sfmodelling/results/ICC/',subject, '_S',num2str(j), '_ICC','f',[2 2 2 2]);
     end
+    ICC_GSR_3D = reshape(ICC_GSR,91,109,91);
+    save_avw(ICC_GSR_3D,strcat('/home/emo4002/colossus_shared3/pons_sfmodelling/results/ICC/',subject, '_S',num2str(j), '_ICC'),'f',[2 2 2 2]);
 end
 
 save('/home/emo4002/colossus_shared3/pons_sfmodelling/results/ICC/ICC_GSR_controlsubjects.mat','ICC_GSR_cat_controls')
 
 %% Calculate mean and standard deviation of ICC across control subjects - from concatenated scans.
 
-load('/home/emo4002/colossus_shared3/pons_sfmodelling/results/ICC/ICC_GSR_controlsubjects.mat', 'ICC_GSR_cat_controls');
+%load('/home/emo4002/colossus_shared3/pons_sfmodelling/results/ICC/ICC_GSR_controlsubjects.mat', 'ICC_GSR_cat_controls');
 cat=[];
 cat_GSR=[];
 for i=24:47
     subject = strcat('SUB', num2str(i));
-    graymask{i} = read_avw(strcat('/home/emo4002/colossus_shared3/CTL_prep/cleanData/',subject,'/avg/wc1c',subject,'_Savg_GM_binarized.nii.gz'));
-    graymask{i}=reshape(graymask{i},[],1); %flattened 1D vector with a 1/0 for each voxel
-
-    disp(num2str(i))
-    ICC_cat = reshape(ICC_final_cat{i}{1}{2}, [], size(ICC_final_cat{i}{1}{2}, 4)); %load so that you have <voxels>
-    ICC_GSR_cat_controls = reshape(ICC_GSR_final_cat{i}{1}{2}, [], size(ICC_GSR_final_cat{i}{1}{2}, 4));
+    GM=read_avw('/home/emo4002/colossus_shared3/c1referenceT1.nii'); 
+    GM_reshape=reshape(GM, [902629 1]);
+    GM_reshape(GM_reshape > 0.25) = 1; %threshold GM mask
+    GM_reshape(GM_reshape <=0.25) = 0;
     
-    ICC_cat(ICC_cat(graymask{i}==1)==0)=NaN; %set values inside the gray mask that have an ICC of zero to be "NaN". This ideally eliminates any problems with the masks not fully lining up between subjects
-    ICC_GSR_cat_controls(ICC_GSR_cat_controls(graymask{i}==1)==0)=NaN;
+    ICC_GSR_cat_controls = reshape(ICC_GSR_final_cat{i}, [], size(ICC_GSR_final_cat{i}, 4));
 
     cat=[cat; ICC_cat'];
     cat_GSR=[cat_GSR;ICC_GSR_cat_controls']; %the img is stored in the 2nd cell. add each new subj as a row.
--- loadScans.m
+++ loadScans.m
@@ -1,11 +1,15 @@
-function cat_func = loadScans(i, j, nscans)
+function cat_func = loadScans(i, j, nscans, sub_direct)
 % Save scans from a single session concatenated together across time.
     cat_func=[];
    
     subject = strcat('SUB', num2str(i));
     for k=1:nscans(j,i) %loop over scans within sessions
         disp(strcat('loading functional scans for:', subject, ', session:', num2str(j), ',scan:', num2str(k)))
-        func=read_avw(strcat('/home/emo4002/colossus_shared3/pons_sfmodelling/stroke_pts/',subject,'/func/S',num2str(j),'/denoise_swaufunc',num2str(k),'.nii')); %91x109x91
+        if i > 23 %controls don't have session directories
+          func=read_avw(strcat(sub_direct,subject,'/func/denoise_swaufunc',num2str(k),'.nii')); %91x109x91
+        else
+          func=read_avw(strcat(sub_direct,subject,'/func/S',num2str(j),'/denoise_swaufunc',num2str(k),'.nii')); %91x109x91
+        end
         func=reshape(func,[], size(func,4)); %flattened 2D matrix that is <voxels>
         if nscans(j,i)==1
             cat_func=func;
