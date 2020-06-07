6049bb76eca029182e04ef6a193a05254f9d4672
emilyolafson <emily.olafson@mail.mcgill.ca>
2020-06-07 17:10:15 -0400

    new versions

---
+++ makeFigures.m
@@ -0,0 +1,14 @@
+function [] = makeFigures(studydir, figuresdir, figs=1:5)
+    if ismember(figs, 1) 
+    % Figure 1 - boxplots of voxels connected vs unconnected to lesion
+    elseif ismember(figs, 2) 
+    % Figure 2 - 
+    elseif ismember(figs, 3)
+    elseif ismember(figs, 4)
+    elseif ismember(figs, 5)
+    elseif ismember(figs, 6)
+    elseif ismember(figs, 7)
+    end
+
+en
+
